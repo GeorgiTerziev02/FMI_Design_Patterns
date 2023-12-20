5282e67967b13e04d2ee029269a0ec28f86e3d75
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-12-21 00:15:17 +0200

    more transformation tests

---
+++ Homeworks/HW 02/DesignPatterns_HW2/DesignPatterns_HW2.Tests/Transformations/DecorateTransformationTests.cs
@@ -0,0 +1,40 @@
+﻿using DesignPatterns_HW2.Transformations;
+
+namespace DesignPatterns_HW2.Tests.Transformations
+{
+    [TestFixture]
+    public class DecorateTransformationTests
+    {
+        [Test]
+        public void TransformShouldDefaultDecoratedText()
+        {
+            // Arrange
+            var text = "text";
+            var transformation = new DecorateTransformation();
+            var expected = "-={ " + text + " }=-";
+
+            // Act
+            var actual = transformation.Transform(text);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TransformShouldDecorateTextWithCustomDecoration()
+        {
+            // Arrange
+            var text = "text";
+            var prefixDecoration = "prefix";
+            var postfixDecoration = "postfix";
+            var transformation = new DecorateTransformation(prefixDecoration, postfixDecoration);
+            var expected = prefixDecoration + " " + text + " " + postfixDecoration;
+
+            // Act
+            var actual = transformation.Transform(text);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Homeworks/HW 02/DesignPatterns_HW2/DesignPatterns_HW2.Tests/Transformations/NormalizeTransformationTests.cs
@@ -0,0 +1,30 @@
+﻿using DesignPatterns_HW2.Transformations;
+
+namespace DesignPatterns_HW2.Tests.Transformations
+{
+    [TestFixture]
+    public class NormalizeTransformationTests
+    {
+        private NormalizeTransformation _normalizeTransformation;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _normalizeTransformation = new NormalizeTransformation();
+        }
+
+        [Test]
+        public void TransformShouldNormalizedText()
+        {
+            // Arrange
+            var expected = "Hello World!";
+            var text = "   Hello   World!   ";
+
+            // Act
+            var actual = _normalizeTransformation.Transform(text);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
