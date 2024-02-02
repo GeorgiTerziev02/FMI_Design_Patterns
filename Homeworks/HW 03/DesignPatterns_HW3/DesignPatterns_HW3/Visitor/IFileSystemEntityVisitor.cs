d05818cbd062ce213e1a6b205ac5649bceab0c08
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-02-02 18:26:24 +0200

    init visitors

---
+++ Homeworks/HW 03/DesignPatterns_HW3/DesignPatterns_HW3/Visitor/IFileSystemEntityVisitor.cs
@@ -0,0 +1,12 @@
+﻿using Directory = DesignPatterns_HW3.FileSystem.Directory;
+using File = DesignPatterns_HW3.FileSystem.File;
+
+namespace DesignPatterns_HW3.Visitor
+{
+    public interface IFileSystemEntityVisitor
+    {
+        void Visit(File file);
+        
+        void Visit(Directory directory);
+    }
+}
---
+++ Homeworks/HW 03/DesignPatterns_HW3/DesignPatterns_HW3/Visitor/ReportWriterVisitor.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns_HW3.Visitor
+{
+    public class ReportWriterVisitor : IFileSystemEntityVisitor
+    {
+        public void Visit(FileSystem.File file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(FileSystem.Directory directory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
