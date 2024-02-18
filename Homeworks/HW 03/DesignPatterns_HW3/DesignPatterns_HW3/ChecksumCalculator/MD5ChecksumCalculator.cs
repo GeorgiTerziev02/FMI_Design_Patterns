0d1191dacae2e4db276a184e16eb51c29d86434b
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-02-18 10:46:59 +0200

    Read and hash on chunks - concept

--- Homeworks/HW 03/DesignPatterns_HW3/DesignPatterns_HW3/ChecksumCalculator/MD5ChecksumCalculator.cs
+++ Homeworks/HW 03/DesignPatterns_HW3/DesignPatterns_HW3/ChecksumCalculator/MD5ChecksumCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.Drawing;
+using System.Security.Cryptography;
 
 namespace DesignPatterns_HW3.ChecksuCalculator
 {
@@ -8,6 +9,28 @@ namespace DesignPatterns_HW3.ChecksuCalculator
         {
             using var md5 = MD5.Create();
             var checksum = md5.ComputeHash(stream);
+            stream.Position = 0;
+            Console.WriteLine(CalculateWithBlocks(stream));
+            return BitConverter.ToString(checksum).Replace("-", string.Empty).ToLower();
+        }
+
+        public string CalculateWithBlocks(Stream stream)
+        {
+            using var md5 = MD5.Create();
+
+            var buffer = new byte[1024];
+            var bytesRead = 0;
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                md5.TransformBlock(buffer, 0, bytesRead, buffer, 0);
+            }
+            md5.TransformFinalBlock(buffer, 0, bytesRead);
+            return ToStringChecksum(md5.Hash);
+        }
+
+        // TODO: maybe move
+        public string ToStringChecksum(byte[] checksum)
+        {
             return BitConverter.ToString(checksum).Replace("-", string.Empty).ToLower();
         }
     }
